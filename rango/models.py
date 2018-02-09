c93dadaf97a71c50bcf92565421d2d630d464611
Wang <2368449W@student.gla.ac.uk>
2018-02-09 14:16:07 +0800

    end of chapter 7

--- rango/models.py
+++ rango/models.py
@@ -12,11 +12,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
-    
-    
 
 
 class Page(models.Model):
